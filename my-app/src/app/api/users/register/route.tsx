2a9299164ed9e7e608de7f4d2867cac552a7fbe8
tareksayedhassan <avakinmemo@gmail.com>
2025-07-07 18:45:46 +0300

    init project front and back

---
+++ my-app/src/app/api/users/register/route.tsx
@@ -0,0 +1,72 @@
+import { NextRequest, NextResponse } from "next/server";
+import { registervalidate } from "@/utils/ValidationSchemas";
+import prisma from "@/utils/db";
+import bcrypt from "bcryptjs";
+import { Role } from "@/generated/prisma";
+import generateJWT from "@/utils/generateJWT";
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+export async function POST(req: NextRequest) {
+  const body = (await req.json()) as User;
+
+  const validation = registervalidate.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(
+      {
+        message: validation.error.issues[0],
+      },
+      { status: 400 }
+    );
+  }
+
+  const data = validation.data;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (user) {
+    return NextResponse.json(
+      {
+        message: "Email aleady exists",
+      },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+        role: data.role === "admin" ? Role.admin : Role.user,
+      },
+    });
+
+    const token = await generateJWT({
+      name: newUser.name,
+      email: newUser.email,
+      role: newUser.role,
+    });
+  } catch (error) {
+    console.log("error", error);
+    return NextResponse.json(
+      {
+        message: "Failed to create user",
+        error: String(error),
+      },
+      { status: 500 }
+    );
+  }
+}
